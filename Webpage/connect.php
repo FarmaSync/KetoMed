16082bae58b05b08c6fa881829f7c5da0e42c00e
FarmaSync <samirelabdouni@gmail.com>
2022-11-06 09:22:54 +0100

    Commit

---
+++ Webpage/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "185.224.137.5";
+$username = "u219831890_ketobeheer";
+$password = "Admin_admin1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
