6d596e298fd652327fe60b9578a30d4b0ff5348e
FarmaSync <samirelabdouni@gmail.com>
2022-11-07 10:55:14 +0100

    commit

--- Webpage/connect.php
+++ Webpage/connect.php
@@ -2,13 +2,13 @@
 $servername = "185.224.137.5";
 $username = "u219831890_ketobeheer";
 $password = "Admin_admin1";
+$databaseName = "u219831890_ketomed";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $databaseName);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 ?>
---
+++ Webpage/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "185.224.137.5";
+$username = "u219831890_ketobeheer";
+$password = "Admin_admin1";
+$databaseName = "u219831890_ketomed";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $databaseName);
+// Check connection
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error(); 
+}
+//echo "Connected successfully";
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <?php include('Webpage\results.html')?>
+</html>
