19800511f1d748dd58f0e43b0debe7298f890e31
FarmaSync <samirelabdouni@gmail.com>
2022-11-07 11:52:23 +0100

    commit

--- Webpage/index.php
+++ Webpage/index.php
@@ -5,6 +5,8 @@ $username = "u219831890_ketobeheer";
 $password = "Admin_admin1";
 $databaseName = "u219831890_ketomed";
 
+echo "Hello world";
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $databaseName);
 // Check connection
@@ -12,10 +14,28 @@ if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error(); 
 }
 //echo "Connected successfully";
+
+$sql = "SELECT * FROM cbg_labeled_20221105
+WHERE WERKZAMESTOFFEN LIKE '%midazol%'
+";
+
+// Make query & get results
+$result = mysqli_query($conn, $sql);
+
+//Fetch resulting rows as an array
+$arrays = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+//free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+print_r($arrays);
+
+
+
 ?>
 
 
-<!DOCTYPE html>
-<html>
-    <?php include('Webpage\results.html')?>
-</html>
