c4582f24325d6b7d9963f901d9c841fe5bfd445f
FarmaSync <samirelabdouni@gmail.com>
2022-11-07 14:55:10 +0100

    connect db in index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-//CONNECT TO DATABASE
+<?php 
+//connect to db
+$servername = "185.224.137.5";
+$username = "u219831890_ketobeheer";
+$password = "Admin_admin1";
+$databaseName = "u219831890_ketomed";
 
+echo "Hello world";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $databaseName);
+// Check connection
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error(); 
+}
+
+?>
 
 
 
