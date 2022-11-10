f432209719278b3eba3d0918aff21ef84c3ad0fd
FarmaSync <samirelabdouni@gmail.com>
2022-11-10 20:51:40 +0100

    s

--- results.php
+++ results.php
@@ -26,6 +26,10 @@
 		<script src=
             "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js">
             </script>
+		<link rel="stylesheet" 
+            type="text/css" href=
+            "https://use.fontawesome.com/releases/v5.6.3/css/all.css"> 
+
 		<!-- Own scripts-->
 		<script type="text/javascript">           
 			function showHideRow(row) 
